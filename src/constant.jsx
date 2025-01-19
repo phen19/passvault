12399752a3ac92afd105f094ec989c98fd395fd7
Paulo Henrique Lopes <phenriquelgsilva@gmail.com>
2025-01-19 13:20:04 -0300

    feat: editing API host in constant file

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,4 +1,4 @@
-const API = "http://sophisticated-muire-projetospessoais-bf19be74.koyeb.app";
+const API = "https://sophisticated-muire-projetospessoais-bf19be74.koyeb.app";
 
 const categorias = [
     {"wifis":[
